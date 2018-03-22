56f8333f4b8b77b87bf41090269d11012b4a8fb7
maxproof <max.karberg@gmail.com>
2018-03-22 10:01:27 +0100

    outputting recipe with ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,6 +10,8 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
     new Recipe('A test Recipe', 'This is simply a test',
+      'https://photos.smugmug.com/Thai-Recipes-2017/i-cbT2RpB/0/f743dc95/X3/thai-cashew-chicken-recipe-30-X3.jpg'),
+      new Recipe('A test Recipe', 'This is simply a test',
       'https://photos.smugmug.com/Thai-Recipes-2017/i-cbT2RpB/0/f743dc95/X3/thai-cashew-chicken-recipe-30-X3.jpg')
   ];
 
